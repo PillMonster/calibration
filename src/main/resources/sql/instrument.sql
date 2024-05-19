56540a26c00558b5b522e448a8d11d66ef92ecdd
PillMonster <user@Dylan>
2024-05-19 10:29:21 +0800

    laptop

--- src/main/resources/sql/instrument.sql
+++ src/main/resources/sql/instrument.sql
@@ -14,21 +14,22 @@ CREATE TABLE instrument (
   calibrate_month VARCHAR(32) COMMENT '校驗月份',
   last_calibrate_date DATE COMMENT '上次校驗時間',
   mother_instrument_number VARCHAR(32) COMMENT '母儀編號',
+  is_calibration VARCHAR(32) COMMENT '是否校驗完成',
   PRIMARY KEY (instrument_id)
 );
 
 INSERT INTO instrument (number, name, type, characteristic, unit, cycle, 
 						calibrate_type, calibrate_localation, calibrate_month, 
-                        last_calibrate_date, mother_instrument_number) 
+                        last_calibrate_date, mother_instrument_number, is_calibration) 
 		VALUES ('CQ119', '游標卡尺', 'Mitutoyo 200mm', '長度', 'mm', 6, 
-				'內校', 'BQC', '1,6', '2024-1-18', '41QBB99303F');
+				'內校', 'BQC', '1,6', '2024-1-18', '41QBB99303F', 'N');
 INSERT INTO instrument (number, name, type, characteristic, unit, cycle, 
 						calibrate_type, calibrate_localation, calibrate_month, 
-                        last_calibrate_date, mother_instrument_number) 
+                        last_calibrate_date, mother_instrument_number, is_calibration) 
 		VALUES ('CQ219', '游標卡尺', 'Mitutoyo 200mm', '長度', 'mm', 6, 
-				'內校', 'BQC', '1,6', '2024-1-18', '41QBB99303F');
+				'內校', 'BQC', '1,6', '2024-1-18', '41QBB99303F', 'N');
 INSERT INTO instrument (number, name, type, characteristic, unit, cycle, 
 						calibrate_type, calibrate_localation, calibrate_month, 
-                        last_calibrate_date, mother_instrument_number) 
+                        last_calibrate_date, mother_instrument_number, is_calibration) 
 		VALUES ('CQ318', '游標卡尺', 'Mitutoyo 200mm', '長度', 'mm', 6, 
-				'內校', 'BQC', '1,6', '2024-1-18', '41QBB99303F');
+				'內校', 'BQC', '1,6', '2024-1-18', '41QBB99303F', 'N');
