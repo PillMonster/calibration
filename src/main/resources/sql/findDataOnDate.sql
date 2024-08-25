677c59c2467dcf7c2ee3aaf7b262067c590b9307
PillMonster <dylan9342@gmail.com>
2024-08-26 00:08:53 +0800

    desktop

--- src/main/resources/sql/findDataOnDate.sql
+++
@@ -1,4 +0,0 @@
-SELECT data.* FROM data
-JOIN instrument_spec_data ON data.data_id = instrument_spec_data.data_id
-JOIN instrument ON instrument_spec_data.instrument_id = instrument.instrument_id
-WHERE instrument_spec_data.instrument_id = '3' AND data.calibrate_date = instrument.last_calibrate_date;
