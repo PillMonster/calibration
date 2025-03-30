4bd85cfd83d8ed5dde89f7b63d49d2c0c49f0339
PillMonster <dylan9342@gmail.com>
2025-03-30 23:36:43 +0800

    desktop

--- src/main/java/chien/myweb/calibration/controller/ShowPageController.java
+++ src/main/java/chien/myweb/calibration/controller/ShowPageController.java
@@ -1,8 +1,14 @@
 package chien.myweb.calibration.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/calibration")
@@ -41,7 +47,14 @@ public class ShowPageController {
 	}
 	
 	@GetMapping("/instrument/crud")
-	public String showInstruments() {
+	public String showInstruments(HttpServletRequest request) {
+		String requestIP = request.getRemoteAddr();
+		
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // 日期格式
+		String requestTime  = df.format(new Date()); // date轉換為String
+		
+		System.out.println("對方請求的IP為: " + requestIP);
+		System.out.println("對方請求的時間為: " + requestTime);
 		return "crudInstrument";
 	}
 	
